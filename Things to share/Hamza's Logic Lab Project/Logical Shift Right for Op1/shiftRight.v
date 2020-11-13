1754c7c9309c8b07661c792a826cfbdfaddb6277
Import3r <jamfosa@protonmail.com>
2020-11-13 15:00:57 +0200

    romve this commit and previous

--- Things to share/Hamza's Logic Lab Project/Logical Shift Right for Op1/shiftRight.v
+++
@@ -1,8 +0,0 @@
-module shiftRight(op1,result);
-input [7:0] op1;
-output reg [7:0] result;
-
-always @* begin
-result = op1 >> 1;
-end
-endmodule
--- Things to share/Hamza's Logic Lab Project/Other modules/testRegs.v
+++
@@ -1,9 +0,0 @@
-module testRegs();
-reg [3:0] x,y,z;
-wire [7:0] o1,o2,o3;
-initial begin
-x<=4'b0000; y<=4'b1000; z<=4'b1111;
-#25 $finish;
-end
-registers r1(x,y,z,o1,o2,o3);
-endmodule
